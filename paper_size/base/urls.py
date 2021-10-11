ae10b160cbb4b310dc7cf69d7857120ee3412277
Maxim Shilov <m.shilov201612@gmail.com>
2021-10-11 09:13:21 +0700

    add create, updete, delete paper. login, logout, reg.

--- paper_size/base/urls.py
+++ paper_size/base/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
-from . import views
+from .views import PaperList, PaperDetail, PaperCreate, PaperUpdate, PaperDelete, LoginView, RegisterView, Home
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
-    path('', views.index, name='home'),
+	path('', Home.as_view(), name='home'),
+
+	path('papers', PaperList.as_view(), name='papers'),
+
+	path('paper/<int:pk>/', PaperDetail.as_view(), name='paper'),
+	path('paper-create/', PaperCreate.as_view(), name='paper-create'),
+	path('paper-update/<int:pk>', PaperUpdate.as_view(), name='paper-update'),
+	path('paper-delete/<int:pk>', PaperDelete.as_view(), name='paper-delete'),
+
+	path('login/', LoginView.as_view(), name='login'),
+	path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+	path('reg/', RegisterView.as_view(), name='register'),
 ]
--- paper_size/base/views.py
+++ paper_size/base/views.py
@@ -1,10 +1,91 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
+from django.urls import reverse_lazy
 from .models import Paper
 
 
-from django.http import HttpResponse
+class Home(ListView):
+	template_name = 'base/home.html'
+	model = Paper
+	context_object_name = 'papers'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['papers'] = context['papers'].filter(public=True)
+		return context
 
 
-def index(request):
-	return HttpResponse('Paper')
+class PaperList(LoginRequiredMixin, ListView):
+	template_name = 'base/PaperList.html'
+	model = Paper
+	context_object_name = 'papers'
+	
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['papers'] = context['papers'].filter(user=self.request.user)
+		return context
+
+ 
+class PaperDetail(DetailView):
+	model = Paper
+	context_object_name = 'paper'
+	template_name = 'base/PaperDetail.html'
+
+
+class PaperCreate(LoginRequiredMixin, CreateView):
+	template_name = 'base/PaperCreate.html'
+	model = Paper
+	fields = ['title', 'description', 'paper', 'public']
+	success_url = reverse_lazy('papers')
+
+	def form_valid(self, form):
+		form.instance.user = self.request.user
+		return super(PaperCreate, self).form_valid(form)
+
+
+class PaperUpdate(LoginRequiredMixin, UpdateView):
+	template_name = 'base/PaperCreate.html'
+	model = Paper
+	fields = ['title', 'description', 'paper', 'public']
+	success_url = reverse_lazy('papers') 
+
+
+class PaperDelete(LoginRequiredMixin, DeleteView):
+	template_name = 'base/PaperDelete.html'
+	model = Paper
+	context_object_name = 'paper'
+	success_url = reverse_lazy('papers')
+
+
+class LoginView(LoginView):
+	template_name = 'base/LoginView.html'
+	fields = '__all__'
+	redirect_authenticated_user = True
+
+	def get_success_url(self):
+		return reverse_lazy('papers')
+
+
+class RegisterView(FormView):
+	template_name = 'base/RegisterView.html'
+	form_class = UserCreationForm
+	redirect_authenticated_user = True
+	success_url = reverse_lazy('papers')
+
+	def form_valid(self, form):
+		user = form.save()
+		if user is not None:
+			login(self.request, user)
+		return super(RegisterView, self).form_valid(form)
+
+	def get(self, *args, **kwargs):
+		if self.request.user.is_authenticated:
+			return redirect('papers')
+		return super(RegisterView, self).get(*args, **kwargs)
